9da32c8c9360578c6de414311d5e843d7508112f
Gustavo Delgado <gustavodelgadoa@outlook.com>
2026-02-19 23:34:57 -0500

    Added imports to classify.py file

---
+++ classify.py
@@ -0,0 +1,8 @@
+# Program written by Gustavo Delgado
+
+import torch                        # Include PyTorch Library
+import torch.nn                     # Include Neural Network Module for building & training Neural Networks
+import torch.nn.functional          # Includes functions such for Activation functions & Loss Functions
+import torch.utils.data             # Includes tools for handling datasets & data loading for PyTorch
+import torchvision                  # Includes tools & functions for computer vision tasks (loading datasets, transforming images, and pre-trained models)
+
