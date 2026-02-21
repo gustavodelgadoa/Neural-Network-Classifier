84894bffd9877da9de9ecc5ff41efa4b8f44fa99
Gustavo Delgado <gustavodelgadoa@outlook.com>
2026-02-20 22:09:41 -0500

    Implemented function for data loading.

--- classify.py
+++ classify.py
@@ -11,12 +11,12 @@ import torchvision                  # Includes tools & functions for computer vi
 def main(): 
 
     print("Starting the program")
-    load_dataset('./data.CIFAR10_Training_Data', True, torchvision.transforms.ToTensor(), True)     # Loads training dataset
-    load_dataset('./data.CIFAR10_Testing_Data', False, torchvision.transforms.ToTensor(), True)     # loads testing dataset
+    download_dataset('/data/CIFAR10_Training_Data', True, torchvision.transforms.ToTensor(), True)     # downloads training dataset
+    download_dataset('/data/CIFAR10_Testing_Data', False, torchvision.transforms.ToTensor(), True)     # downloads testing dataset
 
 
 
-def load_dataset(destination_folder, data_set, transform_type, download):
+def download_dataset(destination_folder, data_set, transform_type, download):
     """
      Creating the dataset object, automatically downloads the data from the Internet & stores it in a data directory located in project root directory/
 
@@ -27,13 +27,33 @@ def load_dataset(destination_folder, data_set, transform_type, download):
         transform_type (function): Converts PIL/numpy to torch.FloatTensor of shape (C x H x W)
         download (boolean): True will download data if not exists at root, False assumes data exists (error if not exists)
     """    
-    training_dataset = torchvision.datasets.CIFAR10(
+    download_dataset = torchvision.datasets.CIFAR10(
         root = destination_folder, 
         train = data_set, 
         transform = transform_type,
         download = download
-    ) # training_dataset
-    return training_dataset
+    ) # download_dataset
+    return download_dataset
+
+def load_data(dataset_name, batch_size_amount, shuffle_order, number_of_workers, drop_last_batches):
+    """
+    Will split dataset objects into smaller chunks (batches), handles shuffling, parallel loading, and iteration automatically.
+
+    Args:
+        dataset_name (String): CIFAR10 object
+        batch_size_amount (integer): number of images to load at once
+        shuffle_order (boolean): True for randomizing order, False will not randomize
+        number_of_workers (integer): number of CPU processes to use for loading data
+        drop_last_batches (boolean): True will drop incomplete batch, False will keep batch (Total images / batch_size_amount = full batches + 1 incomplete batch)
+    """    
+    load_data = torch.utils.data.DataLoader(
+        dataset = dataset_name, 
+        batch_size = batch_size_amount,
+        shuffle = shuffle_order, 
+        num_workers = number_of_workers, 
+        drop_last = drop_last_batches
+    ) # load_data 
+    return load_data
 
 
 
