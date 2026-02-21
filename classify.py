2cad5281adddb308ddd3ea3bcf2712dd7168c8d9
Gustavo Delgado <gustavodelgadoa@outlook.com>
2026-02-20 21:11:59 -0500

    Implemented load_dataset function which is used for loading both training & test datasets.

--- classify.py
+++ classify.py
@@ -9,46 +9,31 @@ import torchvision                  # Includes tools & functions for computer vi
 
 
 def main(): 
-    """
-    
-    Args: 
 
-    Returns: 
-
-    Raises: 
-    
-    """
     print("Starting the program")
-    load_training_dataset()
+    load_dataset('./data.CIFAR10_Training_Data', True, torchvision.transforms.ToTensor(), True)     # Loads training dataset
+    load_dataset('./data.CIFAR10_Testing_Data', False, torchvision.transforms.ToTensor(), True)     # loads testing dataset
 
 
-############################ Training dataset #################################
-## The torchvision.datasets contains pre-built dataset classes, where CIFAR-10 is one of them.
-## Creating the dataset object, automatically downloads the data from the Internet & stores it in a data directory located in project root directory/
 
-# torchvision.datasets.CIFAR10(root, train, )
-def load_training_dataset(destination_folder, boolean, transform_type, download):
+def load_dataset(destination_folder, data_set, transform_type, download):
     """
-    """
+     Creating the dataset object, automatically downloads the data from the Internet & stores it in a data directory located in project root directory/
+
+
+    Args:
+        destination_folder (String): Location for dataset to be stored
+        data_set (boolean): True to get training dataset, False to get test dataset
+        transform_type (function): Converts PIL/numpy to torch.FloatTensor of shape (C x H x W)
+        download (boolean): True will download data if not exists at root, False assumes data exists (error if not exists)
+    """    
     training_dataset = torchvision.datasets.CIFAR10(
         root = destination_folder, 
-        train = boolean, 
+        train = data_set, 
         transform = transform_type,
         download = download
     ) # training_dataset
-
-
-
-
-############################ Testing dataset #################################
-## The torchvision.datasets contains pre-built dataset classes, where CIFAR-10 is one of them.
-## Creating the dataset object, automatically downloads the data from the Internet & stores it in a data directory located in project root directory/
-
-def load_testing_dataset(): 
-    testing_dataset = torchvision.datasets.CIFAR10( 
-
-
-    ) # testing_dataset
+    return training_dataset
 
 
 
